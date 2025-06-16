f3a3f63e23e35cb632ad6d401c34b8116e76e021
Subhrajit Dhar <108873732+devwithsubhrajit2002@users.noreply.github.com>
2025-06-16 19:50:31 +0530

    commit new new

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Driver from './Driver';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/leafletFix.js
+++ src/leafletFix.js
@@ -1,10 +1,24 @@
+// import L from 'leaflet';
+// import iconUrl from 'leaflet/dist/images/marker-icon.png';
+// import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// delete L.Icon.Default.prototype._getIconUrl;
+
+// L.Icon.Default.mergeOptions({
+//   iconUrl,
+//   shadowUrl: iconShadow,
+// });
+// /client/src/leafletFix.js
+
 import L from 'leaflet';
-import iconUrl from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconUrl,
-  shadowUrl: iconShadow,
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
 });
